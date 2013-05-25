015f2c7202b61bca4220a3b9b7e26e340c01a323
Jaromir Coufal <jcoufal@redhat.com>
2013-05-25 18:45:14 +0200

    Initial settingup and styling

--- app/views/example.html.js
+++ app/views/example.html.js
@@ -1,7 +1,42 @@
 angular.module("alch-templates").run(function($templateCache) {
   $templateCache.put("app/views/example.html",
     "<div>" +
-    "  <h3 example-directive></h3>" +
+    "  <h1>Context Menus</h1>" +
+    "" +
+    "  <ul class=\"context_menu\">" +
+    "    <li><a href=\"#\">Item 1</a></li>" +
+    "    <li><a href=\"#\">Item 2</a></li>" +
+    "    <li><a href=\"#\">Item 3</a></li>" +
+    "    <li><a href=\"#\">Item 4</a></li>" +
+    "" +
+    "    <li class=\"divider\"></li>" +
+    "" +
+    "    <li><a href=\"#\"><img src=\"images/icon.png\"/></a></li>" +
+    "    <li><a href=\"#\">Text</a></li>" +
+    "    <li><a href=\"#\"><img src=\"images/icon.png\"/> Text</a></li>" +
+    "" +
+    "    <li class=\"divider\"></li>" +
+    "" +
+    "    <li class=\"context_submenu pull_right\">" +
+    "      <a href=\"#\">Submenu right</a>" +
+    "      <ul class=\"context_menu\">" +
+    "        <li><a href=\"#\">Item 1</a></li>" +
+    "        <li><a href=\"#\">Item 2</a></li>" +
+    "        <li><a href=\"#\">Item 3</a></li>" +
+    "        <li><a href=\"#\">Item 4</a></li>" +
+    "      </ul>" +
+    "    </li>" +
+    "" +
+    "    <li class=\"context_submenu pull_left\">" +
+    "      <a href=\"#\">Submenu left</a>" +
+    "      <ul class=\"context_menu\">" +
+    "        <li><a href=\"#\">Item 1</a></li>" +
+    "        <li><a href=\"#\">Item 2</a></li>" +
+    "        <li><a href=\"#\">Item 3</a></li>" +
+    "        <li><a href=\"#\">Item 4</a></li>" +
+    "      </ul>" +
+    "    </li>" +
+    "  </ul>" +
     "</div>" +
     "");
 });
